17de9fb34f6b9b516c2aaecbc6f5f01ce1576490
sz3jdii <swiatkowskiam@gmail.com>
2022-01-09 20:30:46 +0100

    Home container

---
+++ home/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="logo" />
+        <p>
+          Realizacja zadania nr 1 w ramach laboratorium PFSwChO - Adam Świątkowski
+        </p>
+        <div className="menu-items">
+          <a
+            href="/"
+          >
+            Home
+          </a>
+          <a
+            href="/calculator"
+          >
+            Calculator
+          </a>
+          <a
+            href="/documentation"
+          >
+            Documentation
+          </a>
+        </div>
+
+      </header>
+    </div> 
+  );
+}
+
+export default App;
